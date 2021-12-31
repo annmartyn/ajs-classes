8876c120ce731a54de38acfea9bbdd7cc3252228
annmartyn <yutamar@yandex.ru>
2021-12-31 17:45:42 +0300

    edit test

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,11 +1,11 @@
 import Character from '../app.js';
 
-test('wrong type', () => {
+test('checking error - wrong type', () => {
     const char = () => new Character('Harry', 'Knight');
     expect(char).toThrow('wrong type');
 });
 
-test('wrong name', () => {
+test('checking error - wrong name', () => {
     const char = () => new Character('HarryCoolestZombie', 'Daemon');
     expect(char).toThrow('wrong name');
 })
